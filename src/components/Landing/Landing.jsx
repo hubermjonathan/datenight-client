dbed0c9fa6c70c826e834c4e5e2443259fe0df85
Jon Huber <hubermjonathan@gmail.com>
2019-09-11 18:16:28 -0400

    added content and tests to the landing page, no design

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Landing.scss';
+import RouterButton from '../../common/RouterButton/RouterButton';
 
-const Landing = () => (
-  <div>
-    <h1 className="title">DateNight</h1>
-  </div>
-);
+function Landing() {
+  return (
+    <div>
+      <h1 className="title">DateNight</h1>
+      <h3 className="description">date ideas simplified.</h3>
+      <RouterButton linkTo="/">get started</RouterButton>
+    </div>
+  );
+}
 
 export default Landing;
--- src/components/Landing/Landing.test.jsx
+++ src/components/Landing/Landing.test.jsx
@@ -6,18 +6,15 @@ import Landing from './Landing';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Landing />', () => {
+  const wrapper = Enzyme.shallow(<Landing />);
+
   it('renders the page title', () => {
-    const wrapper = Enzyme.shallow(<Landing />);
-    expect(wrapper.find('.title').length).toBe(1);
+    expect(wrapper.find('.title')).toHaveLength(1);
+    expect(wrapper.find('.title').text()).toMatch('DateNight');
   });
 
   it('renders the description', () => {
-    const wrapper = Enzyme.shallow(<Landing />);
-    expect(wrapper.find('.description').length).toBe(1);
-  });
-
-  it('renders the continue button', () => {
-    const wrapper = Enzyme.shallow(<Landing />);
-    expect(wrapper.find('.button').length).toBe(1);
+    expect(wrapper.find('.description')).toHaveLength(1);
+    expect(wrapper.find('.description').text()).toMatch('date ideas simplified');
   });
 });
