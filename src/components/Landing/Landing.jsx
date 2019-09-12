af3f73a06000765a06bfe37d18e9dc9c25402212
Jon Huber <hubermjonathan@gmail.com>
2019-09-12 01:51:16 -0400

    implemented styling for landing page, last commit before branches

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,10 +4,12 @@ import RouterButton from '../../common/RouterButton/RouterButton';
 
 function Landing() {
   return (
-    <div>
-      <h1 className="title">DateNight</h1>
-      <h3 className="description">date ideas simplified.</h3>
-      <RouterButton linkTo="/">get started</RouterButton>
+    <div className="landing">
+      <div className="title">DateNight</div>
+      <div className="description">date ideas simplified.</div>
+      <div className="button">
+        <RouterButton linkTo="/form">get started</RouterButton>
+      </div>
     </div>
   );
 }
