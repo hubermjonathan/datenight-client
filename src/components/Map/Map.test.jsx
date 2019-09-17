eaf15b86612d93d12e7c79735d348be17c12e639
Jon Huber <hubermjonathan@gmail.com>
2019-09-17 01:11:03 -0400

    fixed test case for new data

--- src/components/Map/Map.test.jsx
+++ src/components/Map/Map.test.jsx
@@ -6,21 +6,43 @@ import Map from './Map';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Map />', () => {
-  const pins = [
-    {
-      location: {
-        lat: -34.397,
-        lng: 150.644,
-      },
+  const pins = {
+    state: {
+      pins: [
+        {
+          location: {
+            lat: 37.250204,
+            lng: -121.844305,
+          },
+          name: 'Happy Lemon',
+          phone: '(408) 622-6785',
+          website: 'postmates.com',
+          hours: 'open until 10pm today',
+        },
+        {
+          location: {
+            lat: 37.321751,
+            lng: -121.971519,
+          },
+          name: '7 Leaves',
+          phone: '(408) 618-8401',
+          website: 'no website',
+          hours: 'open until 11pm today',
+        },
+        {
+          location: {
+            lat: 37.420849,
+            lng: -121.916505,
+          },
+          name: 'Fantasia',
+          phone: '(408) 260-1668',
+          website: 'doordash.com',
+          hours: 'open until 10pm today',
+        },
+      ],
     },
-    {
-      location: {
-        lat: -34.396,
-        lng: 150.645,
-      },
-    },
-  ];
-  const wrapper = Enzyme.shallow(<Map pins={pins} />);
+  };
+  const wrapper = Enzyme.shallow(<Map location={pins} />);
 
   it('renders the map container', () => {
     expect(wrapper.find('.mapContainer')).toHaveLength(1);
