3384464e23cfe15f43b93e1d9d29e8dfc868f5e3
Huberrrr <hubermjonathan@gmail.com>
2019-09-19 21:46:44 -0400

    improved card generation

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React from 'react';
 import './List.scss';
 import Nav from '../../common/Nav/Nav';
 
@@ -7,33 +7,37 @@ function List(props) {
   const { location } = props;
   const { results } = location.state;
 
-  const createdCards = [];
-  for (let i = 0; i < results.length; i += 1) {
-    createdCards.push(
-      <div className="listCard">
-        <div className="listCardTitle">{results[i].name}</div>
-        {results[i].rating
-          && (
-            <div className="listCardRating">
-              {results[i].rating}
-              {' '}
-              stars
-            </div>
-          )}
-        <div className="listCardAddress">{results[i].address}</div>
-        <div className="listCardPhone">{results[i].phone}</div>
-        <a className="listCardWebsite" href={results[i].website}>visit their website</a>
-      </div>,
-    );
+  function getCards() {
+    const createdCards = [];
+    if (results) {
+      for (let i = 0; i < results.length; i += 1) {
+        createdCards.push(
+          <div className="listCard" key={`listCard${i}`}>
+            <div className="listCardTitle">{results[i].name}</div>
+            {results[i].rating
+              && (
+                <div className="listCardRating">
+                  {results[i].rating}
+                  {' '}
+                  stars
+                </div>
+              )}
+            <div className="listCardAddress">{results[i].address}</div>
+            <div className="listCardPhone">{results[i].phone}</div>
+            <a className="listCardWebsite" href={results[i].website}>visit their website</a>
+          </div>,
+        );
+      }
+    }
+    return createdCards;
   }
-  const [cards] = useState([...createdCards]);
 
   return (
     <div className="list">
       <Nav currentPage="list" />
 
       <div className="cardsContainer">
-        {cards}
+        {getCards()}
       </div>
     </div>
   );
