3998b73e4cb8bc98bf9ca6291c1d8d33d5275580
Jon Huber <hubermjonathanibm.com@Jons-MacBook-Pro.local>
2019-10-05 15:14:07 -0400

    started writing tests

--- src/components/DatePlanDetail/DatePlanDetail.test.jsx
+++ src/components/DatePlanDetail/DatePlanDetail.test.jsx
@@ -6,9 +6,28 @@ import DatePlanDetail from './DatePlanDetail';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<DatePlanDetail />', () => {
-  const wrapper = Enzyme.shallow(<DatePlanDetail />);
+  const wrapper = Enzyme.shallow(<DatePlanDetail match={{ params: { id: 1 } }} />);
 
-  it('passes', () => {
-    expect(wrapper.find('.detail')).toHaveLength(1);
+  it('renders the dateplan title', () => {
+    expect(wrapper.find('.detailTitle')).toHaveLength(1);
+    // expect dateplan title to be correct
+  });
+
+  it('renders the dateplan rating', () => {
+    expect(wrapper.find('.detailRating')).toHaveLength(1);
+    // expect dateplan rating to be correct
+  });
+
+  it('renders the dateplan item cards', () => {
+    // expect(wrapper.find('.detailCard')).toHaveLength(10);
+  });
+
+  it('renders one of the cards correctly', () => {
+    // isolate one of the cards
+    // check the title
+    // check the rating
+    // check the address
+    // check the phone
+    // check the website
   });
 });
