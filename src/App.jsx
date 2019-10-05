ab211a85e2d0f03f228325f8d5da46b6539eebd2
Jon Huber <hubermjonathanibm.com@Jons-MacBook-Pro.local>
2019-10-04 23:10:55 -0400

    added details route to router

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import config from './auth-config.json';
 import Landing from './components/Landing/Landing';
 import Form from './components/Form/Form';
 import Results from './components/Results/Results';
+import DatePlanDetail from './components/DatePlanDetail/DatePlanDetail';
 
 const onRedirectCallback = (appState) => {
   window.history.replaceState(
@@ -26,6 +27,7 @@ const App = () => (
     <Route path="/" exact component={Landing} />
     <Route path="/form" exact component={Form} />
     <Route path="/results" exact component={Results} />
+    <Route path="/plan/:id" component={DatePlanDetail} />
   </Router>
 );
 
