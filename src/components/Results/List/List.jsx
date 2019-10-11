deadccc9ca5c9cdb9a073259f18407b40dab2ea2
Raymond Truong <raylintruong@gmail.com>
2019-10-10 20:08:33 -0400

    working on sorting

--- src/components/Results/List/List.jsx
+++ src/components/Results/List/List.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import './List.scss';
 
 function List(props) {
   const { results } = props;
+  const [sortChange, setSortChange] = useState('');
 
   function getCards() {
     const createdCards = [];
@@ -36,9 +37,45 @@ function List(props) {
     return createdCards;
   }
 
+  const [cards, setCards] = useState(getCards());
+
+  function onchangeSort(event) {
+    console.log('lmao');
+    setSortChange(event.target.value);
+    const newCards = cards.slice(0);
+    if (sortChange === 'rating') {
+      console.log(newCards);
+      // newCards.sort((e1, e2) => {
+      //   if (e1.) {
+      //     return -1;
+      //   }
+      //   if () {
+      //     return 1;
+      //   }
+      //   return 0;
+      // });
+    }
+    if (sortChange === 'price') { newCards.sort(); }
+    setCards(newCards);
+  }
+
   return (
-    <div className="cardsContainer">
-      {getCards()}
+    <div>
+      <div className="utilityBar">
+        <button className="button" type="button" disabled>
+          Sort By:
+          {' '}
+          <select className="sort" onChange={onchangeSort} value={sortChange}>
+            <option value="" disabled hidden>Default</option>
+            <option value="price">Price</option>
+            <option value="rating">Rating</option>
+          </select>
+        </button>
+      </div>
+
+      <div className="cardsContainer">
+        {cards}
+      </div>
     </div>
   );
 }
